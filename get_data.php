3a334919c4825a9f2fb6bc003b950c93c25c87bf
isreal oyinloye <isreal.oyinloye11@gmail.com>
2020-11-27 19:12:32 +0100

    PHP Output Result

--- get_data.php
+++ get_data.php
@@ -1,3 +1,17 @@
+<style>
+table {
+  width: 100%;
+  border-collapse: collapse;
+}
+
+table, td, th {
+  border: 1px solid black;
+  padding: 5px;
+}
+
+th {text-align: left;}
+</style>
+
 <?php
 
 
@@ -29,8 +43,40 @@ if(isset($_POST['fullname'])){
 //--x------  DATABASE RESULT -----x--
 
     if($result == true){
-        echo "<h3> Successfully Registered. </h3>";
+        echo "<h3>
+         Successfully Registered. 
+         </h3>";
     }
+
+    mysqli_select_db($conx,"ajax_demo");
+$sql2="SELECT * FROM clientintakeform";
+$result2 = mysqli_query($conx,$sql2);
+
+echo "<table> <tr>
+<th>Id</th>
+<th>FullName</th>
+<th>CompanyName</th>
+<th>Website</th>
+<th>Address</th>
+<th>State</th>
+<th>Country</th>
+<th>City</th>
+<th>Postal Code</th>
+</tr>";
+while($row = mysqli_fetch_array($result2)) {
+  echo "<tr>";
+  echo "<td>" . $row['Id'] . "</td>";
+  echo "<td>" . $row['FullName'] . "</td>";
+  echo "<td>" . $row['CompanyName'] . "</td>";
+  echo "<td>" . $row['website'] . "</td>"; 
+  echo "<td>" . $row['address'] . "</td>";
+  echo "<td>" . $row['state'] . "</td>";
+  echo "<td>" . $row['country'] . "</td>"; 
+  echo "<td>" . $row['city'] . "</td>";
+  echo "<td>" . $row['postalCode'] . "</td>";
+  echo "</tr>";
+}
+echo "</table>";
 
 }
 ?>
